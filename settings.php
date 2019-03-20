e983822a65854df2833d1a56b2b2717678596f56
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-03-20 12:06:27 +0100

    Added PDO connection
    Added a PDO connection to the database for the settings.php for all of the pages.

--- settings.php
+++ settings.php
@@ -1,3 +1,11 @@
 <?php
-//Database settings.
+$dsn = 'mysql:dbname=thewall;host=127.0.0.1';
+$user = 'user';
+$password = 'root';
+
+try {
+    $database = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
 ?>
