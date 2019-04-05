cee419e1f48b357f93a11356e44ef7bf52e9d583
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-04-05 18:43:36 +0200

    Added tag search functionality
    Added a function where you can search for a tag and it displays all images with that tag.

---
+++ php_tools/display.php
@@ -0,0 +1,44 @@
+<?php
+$imageID = $image_results['id'];
+$userID = $image_results['user_id'];
+
+//Get tag names
+$sql2 = "SELECT tags.tag_name FROM tags  LEFT JOIN tags  ON tags.tag_name = image_tags.tag_id WHERE image_tags.image_id = ?";
+$tagArray = array();
+$sth = $database->prepare($sql2);
+$sth->execute([$imageID]);
+$tagArray = $sth->fetchAll(PDO::FETCH_COLUMN);
+
+//Get owner name
+$sql3 = "SELECT user_name FROM users WHERE id = ?";
+$sth2 = $database->prepare($sql3);
+$sth2->execute([$userID]);
+$username = null;
+$username = $sth2->fetchColumn();
+?>
+<div class="galleryItem">
+    <img class="galleryItemImg modalButton" src="images/<?php echo $image_results['image_name']; ?>"alt="Picture: <?php echo $image_results['image_title']; ?>"/>
+    <h3 class="galleryItemTitle"><?php echo $image_results['image_title'];?></h3>
+    <div class="modalContent">
+        <div class="modalItemTitle">
+            <h1><?php echo $image_results['image_title'];?></h1>
+        </div>
+        <div class="modalItemRight">
+            <div class="modalItemImg">
+                <img src="images/<?php echo $image_results['image_name'];?>"alt="Picture: <?php echo $image_results['image_title'];?>">
+            </div>
+        </div>
+        <div class="modalItemLeft">
+            <p class="modalItemDesc"> <?php echo $image_results['image_description'];?></p>
+            <h3 class="modalItemOwner">Uploaded by:<br><?php echo $username;?></h3>
+            <h6 class="modalItemDate"><?php echo $image_results['image_date'];?></h6>
+            <!--               Need to finish search functionality for following for loop to be completed.-->
+            <div class="modalItemTags"> <?php for($i=0;$i<sizeof($tagArray);$i++){ ?>
+                    <a class="modalItemTags" href="index.php/search">
+                        <?php echo $tagArray[$i];?>
+                    </a>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
--- php_tools/query.php
+++ php_tools/query.php
@@ -1,52 +1,29 @@
 <?php
-$sql = "SELECT * FROM images";
-foreach ($database->query($sql) as $image_results) {
-    $imageID = $image_results['id'];
-    $userID = $image_results['user_id'];
-
-    //Get tag names
-    $sql2 = "SELECT t.tag_name FROM image_tags it LEFT JOIN tags t ON t.tag_id = it.tag_id WHERE it.image_id = ?";
-    $tagArray = array();
-    $sth = $database->prepare($sql2);
-    $sth->execute([$imageID]);
-    $tagArray = $sth->fetchAll(PDO::FETCH_COLUMN);
-
-    //Get owner name
-    $sql3 = "SELECT user_name FROM users WHERE id = ?";
-    $sth2 = $database->prepare($sql3);
-    $sth2->execute([$userID]);
-    $username = null;
-    $username = $sth2->fetchColumn();
-    ?>
-    <div class="galleryItem">
-        <img class="galleryItemImg modalButton" src="images/<?php echo $image_results['image_name']; ?>"alt="Picture: <?php echo $image_results['image_title']; ?>"/>
-        <h3 class="galleryItemTitle"><?php echo $image_results['image_title'];?></h3>
-        <div class="modalContent">
-
-            <div class="modalItemTitle">
-                <h1><?php echo $image_results['image_title'];?></h1>
-            </div>
-
-            <div class="modalItemRight">
-                <div class="modalItemImg">
-                    <img src="images/<?php echo $image_results['image_name'];?>"alt="Picture: <?php echo $image_results['image_title'];?>">
-                </div>
-            </div>
-
-            <div class="modalItemLeft">
-                <p class="modalItemDesc"> <?php echo $image_results['image_description'];?></p>
-                <h3 class="modalItemOwner">Uploaded by:<br><?php echo $username;?></h3>
-                <h6 class="modalItemDate"><?php echo $image_results['image_date'];?></h6>
-                <!--               Need to finish search functionality for following for loop to be completed.-->
-                <div class="modalItemTags"> <?php for($i=0;$i<sizeof($tagArray);$i++){ ?>
-                    <a class="modalItemTags" href="index.php/search">
-                        <?php echo $tagArray[$i];?>
-                    </a>
-                    <?php } ?>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-<?php } ?>
+if(isset($_GET['tag'])){
+    $tagName = $_GET['tag'];
+    $sql = "SELECT tag_id FROM tags WHERE tag_name = ?";
+    $sth = $database->prepare($sql);
+    $sth->execute([$tagName]);
+    $result = $sth->fetchAll();
+    foreach ($result as $tag_results) {
+        $sqlsss = "SELECT * FROM images  LEFT JOIN image_tags  ON images.id = image_tags.image_id WHERE image_tags.tag_id = ?";
+        $sth = $database->prepare($sqlsss);
+        $sth->execute([$tag_results['tag_id']]);
+        $resultss = $sth->fetchAll();
+        foreach ($resultss as $image_results){
+            include('display.php');
+        }
+    }
+}
+elseif(isset($_GET['user'])){
+    $sql = "SELECT * FROM images WHERE user_id = ?";
+}
+else {
+    $sql = "SELECT * FROM images";
+    $sth = $database->prepare($sql);
+    $sth->execute();
+    $result = $sth->fetchAll();
+    foreach ($result as $image_results) {
+        include('display.php');
+    }
+}
