8db8270a6be7fcc45297c795d2d4d771315e594e
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-04-07 15:54:03 +0200

    Completed search functionality
    Completed the search function for title, tag and user.

--- js/search.js
+++ js/search.js
@@ -1,4 +1,16 @@
-$('#searchButton').click(function() {
+function searchFunction(){
+    var selectOption = document.getElementById("searchMenuSelect").value;
     var searchQuery = document.getElementById("searchInput").value;
-    window.location = "index.php?tag=" + searchQuery;
-});
+
+    switch (selectOption) {
+        case 'title':
+            window.location = "index.php?title=" + searchQuery;
+            break;
+        case 'tag':
+            window.location = "index.php?tag=" + searchQuery;
+            break;
+        case 'user':
+            window.location = "index.php?user=" + searchQuery;
+            break;
+    }
+}
