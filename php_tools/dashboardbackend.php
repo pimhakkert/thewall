cc69a4fd44cd133b94ac5b2d4e77161e20b10b3d
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-04-09 14:54:01 +0200

    Finished username and email edit

--- php_tools/dashboardbackend.php
+++ php_tools/dashboardbackend.php
@@ -62,17 +62,37 @@ if('POST' === $_SERVER['REQUEST_METHOD']){
     }
 
     elseif ($edit) {
+        $error2 = false;
         $newUser = $_POST['newUsername'];
         $newEmail = $_POST['newEmail'];
         $newUser = filter_var($newUser,FILTER_SANITIZE_STRING);
         $newEmail = filter_var($newEmail,FILTER_SANITIZE_STRING);
-        $sql = "SELECT id FROM users WHERE user_name = ?";
-        $sth = $database->prepare($sql);
-        $sth->execute([$username]);
-        $userID = $sth->fetchColumn();
 
-        if(/*username taken*/){
+        $stmt0 = $database->prepare('SELECT * FROM users WHERE user_name=?');
+        $stmt0->execute([$_SESSION['username']]);
+        $userStuff = $stmt0->fetch(PDO::FETCH_ASSOC);
 
+        $stmt = $database->prepare('SELECT user_name FROM users WHERE user_name=?');
+        $stmt->execute([$newUser]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt2 = $database->prepare('SELECT user_email FROM users WHERE user_email=?');
+        $stmt2->execute([$newEmail]);
+        $row2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+        if($row&&strtoupper($userStuff['user_name'])!=strtoupper($_SESSION['username'])){
+            array_push($errorsArray,'<p id="error">This username is already used!</p>');
+            $error2 = true;
+        }
+        if($row2&&strtoupper($userStuff['user_email'])!=strtoupper($newEmail)){
+            array_push($errorsArray,'<p id="dashboardError">This e-mail address is already used!</p>');
+            $error2 = true;
+        }
+        if(!filter_var($newEmail, FILTER_VALIDATE_EMAIL)) {
+            array_push($errorsArray,'<p id="error">This e-mail address isn\'t real!</p>');
+            $error2 = true;
+        }
+
+        if($error2){
+            echo 'Error';
         }
         else {
             $insertSql = "UPDATE users SET user_name = ?, user_email = ?  WHERE user_name = ?";
