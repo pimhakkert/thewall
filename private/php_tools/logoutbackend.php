f406f23c792e8d457051839e4f7ca9e03f65ccbb
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-04-21 12:00:32 +0200

    Hotfix

--- private/php_tools/logoutbackend.php
+++ private/php_tools/logoutbackend.php
@@ -3,11 +3,11 @@ session_start();
 if(isset($_SESSION['username'])){
     session_unset();
     session_destroy();
-    echo "<meta http-equiv='refresh' content='0;url=../../thewall/index.php'>";
+    echo "<meta http-equiv='refresh' content='0;url=../../public_html/thewall/index.php'>";
     exit();
 }
 else {
-    echo "<meta http-equiv='refresh' content='0;url=../../thewall/index.php'>";
+    echo "<meta http-equiv='refresh' content='0;url=../../thewall/public_html/index.php'>";
     exit();
 }
 ?>
