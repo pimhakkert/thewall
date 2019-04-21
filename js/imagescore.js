142e1c321894018b020713005044ebaf8e563ca9
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-04-21 13:54:12 +0200

    Debug score

--- js/imagescore.js
+++ js/imagescore.js
@@ -64,6 +64,14 @@ function scoreImage(imageID,user,upDown) {
                         downvote.className = "galleryItemDownvoteClear";
                         break;
                 }
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                console.log('jqXHR:');
+                console.log(jqXHR);
+                console.log('textStatus:');
+                console.log(textStatus);
+                console.log('errorThrown:');
+                console.log(errorThrown);
             }
         });
     }
